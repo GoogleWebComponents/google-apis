c040974f5642a3d34296c66653a1b419fe245121
Allen Robinson <arobins@google.com>
2018-10-26 10:35:31 -0400

    Rerun polymer-modulizer on the latest code from master. Clean up some comments.

--- google-realtime-api.js
+++ google-realtime-api.js
@@ -7,20 +7,16 @@ The complete set of contributors may be found at https://polymer.github.io/CONTR
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at https://polymer.github.io/PATENTS.txt
 */
+import '@polymer/polymer/polymer-legacy.js';
+
+import { IronJsonpLibraryBehavior } from '@polymer/iron-jsonp-library/iron-jsonp-library.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+
 /*
 Dynamically loads the Google Drive Realtime API, firing the `api-load` event when ready.
 
 Any number of components can use `<google-realtime-api>` elements, and the library will only be loaded once.
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-import '@polymer/polymer/polymer-legacy.js';
-
-import { IronJsonpLibraryBehavior } from '@polymer/iron-jsonp-library/iron-jsonp-library.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 Polymer({
 
   is: 'google-realtime-api',
@@ -55,7 +51,7 @@ Polymer({
    * Returns `gapi.drive.realtime`
    */
   get api() {
-    return gapi.drive.realtime;
+    return window.gapi.drive.realtime;
   }
 
 });
